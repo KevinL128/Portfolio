9c7c94a6188fc5ed8cae08ffc04d4344ff96cbca
Kevin Li Ye <142710218+KevinL128@users.noreply.github.com>
2025-01-05 17:19:45 -0500

    Superstore dataset

--- Netflix Movies and TV Shows dataset.sql
+++
@@ -1,9 +0,0 @@
-/* Dataset information 
-
-This project uses the dataset "Netflix Movies and TV Shows" from kaggle.
-The dataset has been imported into an SQL database for analysis.
-
-**Source**: https://www.kaggle.com/datasets/shivamb/netflix-shows
-*/
-
-CREATE DATABASE netflix_titles
