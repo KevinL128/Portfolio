3dfaa7c9815c4f78292be2f81e90335e7c4f0b1c
Kevin Li Ye <142710218+KevinL128@users.noreply.github.com>
2025-01-05 15:24:43 -0500

    Update Netflix Movies and TV Shows dataset.sql

--- Netflix Movies and TV Shows dataset.sql
+++ Netflix Movies and TV Shows dataset.sql
@@ -1,6 +1,8 @@
 /* Dataset information 
+
 This project uses the dataset "Netflix Movies and TV Shows" from kaggle.
 The dataset has been imported into an SQL database for analysis.
+
 **Source**: https://www.kaggle.com/datasets/shivamb/netflix-shows
 */
 
